8dd91c77ca967b3e1137e1ea232a51607c4ed20a
Samantha Withanage <samantha@frostdigital.se>
2023-03-04 09:26:58 +0200

    initial commit

---
+++ App.tsx
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from "react";
+import {Text, View} from "react-native";
+
+const App = () => {
+  // const isDarkMode = useColorScheme() === 'dark';
+
+  // const backgroundStyle = {
+  //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  //   flex: 1,
+  // };
+
+  return (
+    // <SafeAreaView style={backgroundStyle}>
+    //   <StatusBar
+    //     barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+    //     backgroundColor={backgroundStyle.backgroundColor}
+    //   />
+    // </SafeAreaView>
+    <View>
+      <Text>Helo world</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ models/ThemeProps.ts
@@ -0,0 +1,7 @@
+export interface ThemeProps {
+  colors: {
+    colorPrimary: string;
+    colorSecondary: string;
+    colorAssent: string;
+  };
+}
